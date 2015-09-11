e61ce9f72d56b2d2643fe9a9c8a6ce007b9c1673
Antonio Fabregat <fabregat.antonio@gmail.com>
2015-09-11 13:42:33 +0100

    level added to identify future target subpathways

--- src/main/java/org/reactome/server/diagram/converter/graph/output/SubpathwayNode.java
+++ src/main/java/org/reactome/server/diagram/converter/graph/output/SubpathwayNode.java
@@ -5,17 +5,27 @@ import java.util.Collection;
 /**
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
  */
-public class SubpathwayNode {
+public class SubpathwayNode implements Comparable<SubpathwayNode> {
 
     public Long dbId;
     public String stId;
     public String displayName;
     public Collection<Long> events; //These are events dbid
 
-    public SubpathwayNode(GraphNode node, Collection<Long> events) {
+    //Level will be used for the shadowing calculation ONLY on the server side
+    //That's the reason why it doesn't get serialised to json but is kept here
+    public transient int level;
+
+    public SubpathwayNode(GraphNode node, Collection<Long> events, int level) {
         this.dbId = node.dbId;
         this.stId = node.stId;
         this.displayName = node.displayName;
         this.events = events;
+        this.level = level;
+    }
+
+    @Override
+    public int compareTo(SubpathwayNode o) {
+        return this.dbId.compareTo(o.dbId);
     }
 }
