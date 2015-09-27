a5de30e29435047a6195540a7f95c486b12c468d
Antonio Fabregat <fabregat.antonio@gmail.com>
2015-09-27 10:53:16 +0100

    Description taken from database. Other minor changes

--- src/main/java/org/reactome/server/diagram/converter/layout/output/NodeAttachment.java
+++ src/main/java/org/reactome/server/diagram/converter/layout/output/NodeAttachment.java
@@ -1,5 +1,7 @@
 package org.reactome.server.diagram.converter.layout.output;
 
+import org.reactome.core.factory.DatabaseObjectFactory;
+import org.reactome.core.model.DatabaseObject;
 import org.reactome.server.diagram.converter.input.model.OrgGkRenderRenderableFeature;
 import org.reactome.server.diagram.converter.util.ShapeBuilder;
 
@@ -7,35 +9,30 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * @author Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
+ * @author Antonio Fabregat <fabregat@ebi.ac.uk>
  */
 @XmlRootElement
 public class NodeAttachment {
-    private Float relativeX;
-    private Float relativeY;
     public Long reactomeId;
     public String label;
     public String description;
     public Shape shape;
 
-    @Deprecated
-    public String renderableClass;
-    @Deprecated
-    public Long trackId;
-
-    public NodeAttachment(OrgGkRenderRenderableFeature obj, Node node){
-        this.relativeX = obj.getRelativeX().floatValue();
-        this.relativeY = obj.getRelativeY().floatValue();
+    public NodeAttachment(OrgGkRenderRenderableFeature obj, Node node) {
         this.label = obj.getLabel();
         this.description = obj.getDescription();
-        this.trackId = obj.getTrackId().longValue();
+
         if (obj.getReactomeId() != null) {
             this.reactomeId = obj.getReactomeId().longValue();
+            DatabaseObject object = DatabaseObjectFactory.getDatabaseObject(reactomeId);
+            this.description = object.getDisplayName();
         }
-        this.renderableClass = obj.getClass().getSimpleName();
 
+        Float relativeX = obj.getRelativeX().floatValue();
+        Float relativeY = obj.getRelativeY().floatValue();
         Coordinate boxCentre = new Coordinate(
-                Math.round( node.prop.x + node.prop.width * relativeX ),
-                Math.round( node.prop.y + node.prop.height * relativeY )
+                Math.round(node.prop.x + node.prop.width * relativeX),
+                Math.round(node.prop.y + node.prop.height * relativeY)
         );
         this.shape = ShapeBuilder.createNodeAttachmentBox(boxCentre, label);
     }
