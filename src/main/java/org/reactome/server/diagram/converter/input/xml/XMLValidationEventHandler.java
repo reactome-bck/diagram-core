2f548a84acdca14eccc4fe4d84ec2880d56d160c
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2015-12-14 16:54:21 +0000

    code clean up

--- src/main/java/org/reactome/server/diagram/converter/input/xml/XMLValidationEventHandler.java
+++ src/main/java/org/reactome/server/diagram/converter/input/xml/XMLValidationEventHandler.java
@@ -22,27 +22,15 @@ public class XMLValidationEventHandler implements ValidationEventHandler{
         return internalLogger;
     }
 
-
     @Override
     public boolean handleEvent(ValidationEvent event) {
-        //TODO use mail API to send an email with the report
-        //TODO CLEAN-UP
         if(internalLogger!=null){
             internalLogger.append("XML Parsing Event.")
-                    .append(" MESSAGE:  " + event.getMessage())
-                    .append(" LINE: " + event.getLocator().getLineNumber())
-                    .append(" COLUMN: " + event.getLocator().getColumnNumber())
-                    .append(" SEVERITY:  " + event.getSeverity());
+                    .append(" MESSAGE:  ").append(event.getMessage())
+                    .append(" LINE: ").append(event.getLocator().getLineNumber())
+                    .append(" COLUMN: ").append(event.getLocator().getColumnNumber())
+                    .append(" SEVERITY:  ").append(event.getSeverity());
         }
-
-//            logger.warn("   XML Parsing Event");
-//            logger.warn("      LINE: " + event.getLocator().getLineNumber());
-//            logger.warn("      COLUMN: " + event.getLocator().getColumnNumber());
-//            logger.warn("      SEVERITY:  " + event.getSeverity());
-//            logger.warn("      MESSAGE:  " + event.getMessage());
-//            logger.warn("      URL:  " + event.getLocator().getURL());
-//            logger.trace(System.lineSeparator());
-
         return true;
     }
 }
