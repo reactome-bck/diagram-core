538303eb4afae39dd30bf2f33d3c57f4d6d76232
Antonio Fabregat <fabregat.antonio@gmail.com>
2017-05-11 16:39:55 +0100

    main identifier added for the list of attributes

--- src/main/java/org/reactome/server/diagram/converter/util/report/LogEntryType.java
+++ src/main/java/org/reactome/server/diagram/converter/util/report/LogEntryType.java
@@ -1,34 +1,43 @@
 package org.reactome.server.diagram.converter.util.report;
 
+import org.apache.commons.lang.ArrayUtils;
+
 /**
  * @author Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
  */
 public enum LogEntryType {
-    RENDERABLECLASS_MISMATCH("RenderableClassMismatch", "#Diagram", "#Entity", "#Name"), //Fixed
-    SCHEMACLASS_MISSING("SchemaClassMissing", "#Diagram", "#Entity", "#Name", "#RenderableClass"),
-    SUBPATHWAY_WITHOUT_PARTICIPANTS("SubpathwayWithoutParticipants", "#Diagram", "#Subpathway", "#SubpathwayName"), //Not Fixed
-    DIAGRAM_EMPTY("DiagramEmpty", "#Diagram"), //Not Fixed
-    MISSING_REACTION("MissingReaction", "#Diagram", "#Event"),
-    VERY_LONG_NAMES("VeryLongNames", "#Entity"), //Not Fixed
-    DUPLICATE_REACTION_PARTS("DuplicateReactionParts", "#Diagram", "#Reaction", "#PointingTwiceTo"),
-    MISSING_STABLEIDS("MissingStableIds", "#Entity"), //Can be fixed with GHOST
-    ISOLATED_GLYPHS("IsolatedGlyphs", "#Diagram", "#Entity", "Name"), //Fixed
-    OVERLAPPING_REACTION_SHAPES_ON_REACTOMECURATOR("OverlappingReactionShapesOnReactomeCurator", "#Diagram", "#Reaction"); //Can be fixed?
+    RENDERABLECLASS_MISMATCH("RenderableClassMismatch", 1, "#Diagram", "#Entity", "#Name"), //Fixed
+    SCHEMACLASS_MISSING("SchemaClassMissing", 1, "#Diagram", "#Entity", "#Name", "#RenderableClass"),
+    OBJECT_NOT_LONGER_IN_DATABASE("ObjectNotLongerInDatabase", 1, "#Diagram", "#Entity"),
+    SUBPATHWAY_WITHOUT_PARTICIPANTS("SubpathwayWithoutParticipants", 1, "#Diagram", "#Subpathway", "#SubpathwayName"), //Not Fixed
+    DIAGRAM_EMPTY("DiagramEmpty", 0, "#Diagram"), //Not Fixed
+    MISSING_REACTION("MissingReaction", 1, "#Diagram", "#Event"),
+    VERY_LONG_NAMES("VeryLongNames", 0, "#Entity"), //Not Fixed
+    DUPLICATE_REACTION_PARTS("DuplicateReactionParts", 1, "#Diagram", "#Reaction", "#PointingTwiceTo"),
+    MISSING_STABLEIDS("MissingStableIds", 0, "#Entity"), //Can be fixed with GHOST
+    ISOLATED_GLYPHS("IsolatedGlyphs", 1, "#Diagram", "#Entity", "Name"), //Fixed
+    OVERLAPPING_REACTION_SHAPES_ON_REACTOMECURATOR("OverlappingReactionShapesOnReactomeCurator", 1, "#Diagram", "#Reaction"); //Can be fixed?
 
 
     private String[] columns;
     private String filename;
+    private int id;
 
-    LogEntryType(String filename, String... columns) {
+    LogEntryType(String filename, int id, String... columns) {
         this.columns = columns;
         this.filename = filename;
+        this.id = id;
     }
 
     public String[] getColumns() {
-        return columns;
+        return (String[]) ArrayUtils.addAll(columns, new String[]{"#Created","#Modified"});
     }
 
     public String getFilename() {
         return filename;
+    }
+
+    public int getId() {
+        return id;
     }
 }
